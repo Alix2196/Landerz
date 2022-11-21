8b1c0469625a2f839376c7d850bd48f63e773e67
Alix Meneses <angelica962008@hotmail.com>
2022-11-21 15:24:38 -0500

    carousel modification glider

--- js/apires.js
+++ js/apires.js
@@ -1,17 +1,38 @@
-window.addEventListener('load', function () {
-    new Glider(document.getElementById('glider'), {
+// Parte de carrusel
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.glider'), {
         slidesToShow: 1,
-        dots: '#dots',
-        draggable: true,
+        dots: '.carrusel__indicadores',
         arrows: {
-            prev: '#glider-prev',
-            next: '#glider-next'
-        }
-    });
-    
-});
+          prev: '.glider-prev',
+          next: '.glider-next'
+        },responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 775,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 'auto',
+                slidesToScroll: 'auto',
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },
+            {
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.1
+              }
+            }
+          ]
+      });
+  })
 
-async function getUsersPosts() {
+  async function getUsersPosts() {
     let respondeUsers = await fetch('https://jsonplaceholder.typicode.com/users');
     let arrayUsers = await respondeUsers.json();
 
@@ -23,41 +44,40 @@ async function getUsersPosts() {
 
 function mostrarData(usersData, postsData) {
 
-    console.log(usersData, postsData);
-    console.log('company name:',usersData[0].company.name)
+  console.log(usersData, postsData);
+  console.log('company name:',usersData[0].company.name)
 
-    let usuario1 = usersData[0].name;
-    let htmlPosts1 = `<p>"${postsData[0].title}?"</p>`;
-    let company = usersData[0].company.name;
-    
-    
-    let aux;
-    for (userData of usersData) {
-        let boy = `<div>
-        <div>
-            <img src="/access/img/person_1.jpg" class=" rounded-circle" alt="Persona numero1">
-        </div>
-        <div>
-            <p>"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita illo amet ipsum,
-                explicabo corrupti asperiores itaque soluta aperiam atque animi Lorem ipsum dolor sit
-                amet consectetur adipisicing elit".</p>
-        </div>
-        <div>
-            <p><b class="namecaro">John Smith</b></p>
-        </div>
-    </div>`;
-    aux = boy + aux;
-    }
+  let usuario1 = usersData[0].name;
+  let htmlPosts1 = `<p>"${postsData[0].title}?"</p>`;
+  let company = usersData[0].company.name;
+  
+  
+  let aux;
+  for (userData of usersData) {
+      let boy = 
+  `<div>
+      <div>
+          <img src="/access/img/person_1.jpg" class=" rounded-circle" alt="Persona numero1">
+      </div>
+      <div>
+          <p>"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita illo amet ipsum,
+              explicabo corrupti asperiores itaque soluta aperiam atque animi Lorem ipsum dolor sit
+              amet consectetur adipisicing elit".</p>
+      </div>
+      <div>
+          <p><b class="namecaro">John Smith</b></p>
+      </div>
+  </div>`;
+  aux = boy + aux;
+  }
 
-    //document.getElementById("carousel").innerHTML = prr;
+  //document.getElementById("carousel").innerHTML = prr;
 
-    document.getElementById("userNombre1").innerHTML = usuario1;
-    document.getElementById("userPosts1").innerHTML = htmlPosts1;
-    document.getElementById("companyName").innerHTML = company;
+  document.getElementById("userNombre1").innerHTML = usuario1;
+  document.getElementById("userPosts1").innerHTML = htmlPosts1;
+  document.getElementById("companyName").innerHTML = company;
 
 
-    
+  
 }
-
 getUsersPosts();
-
