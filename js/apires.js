d5cf5e6d6709718d5cc6a57870b34172e3c25c1f
Alix Meneses <angelica962008@hotmail.com>
2022-11-22 20:38:25 -0500

    carousel organization and comment

--- js/apires.js
+++ js/apires.js
@@ -1,95 +1,73 @@
 // Parte de carrusel
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider'), {
-        slidesToShow: 1,
-        dots: '.carrusel__indicadores',
-        arrows: {
-          prev: '.glider-prev',
-          next: '.glider-next'
-        },responsive: [
-            {
-              // screens greater than >= 775px
-              breakpoint: 775,
-              settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: 'auto',
-                slidesToScroll: 'auto',
-                itemWidth: 150,
-                duration: 0.25
-              }
-            },
-            {
-              // screens greater than >= 1024px
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                itemWidth: 150,
-                duration: 0.1
-              }
+window.addEventListener('load', function () {
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    dots: '.carrusel__indicadores',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }, responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 775,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 'auto',
+          slidesToScroll: 'auto',
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.1
+        }
+      }
+    ]
+  });
+})
+
+// axios
+async function getApiRest() {
+  try {
+    const responseUser = await axios.get('https://jsonplaceholder.typicode.com/users');
+    const responsePosts = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+    console.log("Respuesta Usuarios ", responseUser.data);
+    console.log("Respuesta complete de posts usuario ", responsePosts.data);
+
+    for(let objUser of responseUser.data){
+      
+      if(objUser.id  <= 4){
+
+        for(let objPosts of responsePosts.data){          
+          if(objUser.id == objPosts.userId){
+            
+            if(objPosts.userId == 3){
+              document.getElementById("namePersona3").innerText = objUser.name;
+              document.getElementById("postsPersona3").innerText = objPosts.body;
+              document.getElementById("emailPersona").innerText = objUser.email;
+            }else if(objPosts.userId == 1){
+              document.getElementById("namePersona1").innerText = objUser.name;
+              document.getElementById("postsPersona1").innerText = objPosts.body;
+            }else if (objPosts.userId == 2){
+            document.getElementById("namePersona2").innerText = objUser.name;
+            document.getElementById("postsPersona2").innerText = objPosts.body;
+            }else if(objPosts.userId == 4){
+            document.getElementById("namePersona4").innerText = objUser.name;
+            document.getElementById("postsPersona4").innerText = objPosts.body;
             }
-          ]
-      });
-  })
-//   const axios = require('axios/dist/browser/axios.cjs'); // browser commonJS bundle (ES2017)
-// // const axios = require('axios/dist/node/axios.cjs'); // node commonJS bundle (ES2017)
-
-  async function getUsersPosts() {
-    let respondeUsers = await fetch('https://jsonplaceholder.typicode.com/users');
-    let arrayUsers = await respondeUsers.json();
-
-    let respondePosts = await fetch('https://jsonplaceholder.typicode.com/posts');
-    let arrayPosts = await respondePosts.json();
-
-    mostrarData(arrayUsers, arrayPosts);
-};
-
-function mostrarData(usersData, postsData) {
-
-  console.log(usersData, postsData);
-  console.log('company name:',usersData[0].company.name)
-
-  console.log("HTML post 1",postsData[0].title);
-
-  let usuario1 = usersData[0].name;
-  let htmlPosts1 = postsData[0].title;
-  let company = usersData[0].company.name;
-  
-  
-  let aux;
-  for (userData of usersData) {
-      let boy = 
-  `<div>
-      <div>
-          <img src="/access/img/person_1.jpg" class=" rounded-circle" alt="Persona numero1">
-      </div>
-      <div>
-          <p>"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita illo amet ipsum,
-              explicabo corrupti asperiores itaque soluta aperiam atque animi Lorem ipsum dolor sit
-              amet consectetur adipisicing elit".</p>
-      </div>
-      <div>
-          <p><b class="namecaro">John Smith</b></p>
-      </div>
-  </div>`;
-  aux = boy + aux;
+          } 
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
   }
-
-  //document.getElementById("carousel").innerHTML = prr;
-
-  document.getElementById("userNombre1").innerHTML = usuario1;
-  document.getElementById("userPosts1").innerHTML = htmlPosts1; //testimonial
-  document.getElementById("companyName").innerHTML = company;
- 
 }
-
-// async function getUser() {
-//     try {
-//       const response = await axios.get('/user?ID=12345');
-//       console.log(response);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-
-getUsersPosts();
+getApiRest();
